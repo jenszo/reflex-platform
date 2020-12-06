9991555b802b9f9850e7e5409d9eff891092b86a
jenszo <>
2020-12-06 15:39:02 +0100

    Bumping nix up to v 2.3.9 (nixpkg requires nix > 2)
    Signed-off-by: jenszo

--- installNix.sh
+++ installNix.sh
@@ -10,42 +10,48 @@ oops() {
     exit 1
 }
 
+umask 0022
+
 tmpDir="$(mktemp -d -t nix-binary-tarball-unpack.XXXXXXXXXX || \
-          oops "Can\'t create temporary directory for downloading the Nix binary tarball")"
+          oops "Can't create temporary directory for downloading the Nix binary tarball")"
 cleanup() {
     rm -rf "$tmpDir"
 }
 trap cleanup EXIT INT QUIT TERM
 
 require_util() {
-    type "$1" > /dev/null 2>&1 || which "$1" > /dev/null 2>&1 ||
+    command -v "$1" > /dev/null 2>&1 ||
         oops "you do not have '$1' installed, which I need to $2"
 }
 
 case "$(uname -s).$(uname -m)" in
-    Linux.x86_64) system=x86_64-linux; hash=d7e3cba2103c051b17d2a49dc6389f96ebe46dca72dac8473cfb87af8b67ef03;;
-    Linux.i?86) system=i686-linux; hash=c9f5c41bea715b72eeab83dd70d5cb42c84469a4b419f918df39ebb8597ffec1;;
-    Linux.aarch64) system=aarch64-linux; hash=6b60342fb430a2842b300a72a9cf179dace569713fb27520513c9fcdf94ba0c4;;
-    Darwin.x86_64) system=x86_64-darwin; hash=b3e1a7316053fda14b4c69142c9f432aab2742e3a5febaed805a316450d6360d;;
+    Linux.x86_64) system=x86_64-linux; hash=49763fd7fa06bcb712ced2f3f11afd275e3a4d7bc5ff0d6fd1d50a4c3ce7bbf4;;
+    Linux.i?86) system=i686-linux; hash=c87c376c2d5277d56664855c668a5dca957c51d1c654849571d12a1b90fbe75a;;
+    Linux.aarch64) system=aarch64-linux; hash=733a26911193fdd44d5d68342075af5924d8c0701aae877e51a38d74ee9f4ff8;;
+    Darwin.x86_64) system=x86_64-darwin; hash=f0f081331b79ce42a638e523c7c0e40ee1aa44641131a7da042230e3a7a2da04;;
+    # eventually maybe: system=arm64-darwin; hash=@binaryTarball_arm64-darwin@;;
+    Darwin.arm64) system=x86_64-darwin; hash=f0f081331b79ce42a638e523c7c0e40ee1aa44641131a7da042230e3a7a2da04;;
     *) oops "sorry, there is no binary distribution of Nix for your platform";;
 esac
 
-url="https://nixos.org/releases/nix/nix-1.11.16/nix-1.11.16-$system.tar.bz2"
+url="https://releases.nixos.org/nix/nix-2.3.9/nix-2.3.9-$system.tar.xz"
 
-tarball="$tmpDir/$(basename "$tmpDir/nix-1.11.16-$system.tar.bz2")"
+tarball="$tmpDir/$(basename "$tmpDir/nix-2.3.9-$system.tar.xz")"
 
 require_util curl "download the binary tarball"
-require_util bzcat "decompress the binary tarball"
 require_util tar "unpack the binary tarball"
+if [ "$(uname -s)" != "Darwin" ]; then
+    require_util xz "unpack the binary tarball"
+fi
 
-echo "downloading Nix 1.11.16 binary tarball for $system from '$url' to '$tmpDir'..."
+echo "downloading Nix 2.3.9 binary tarball for $system from '$url' to '$tmpDir'..."
 curl -L "$url" -o "$tarball" || oops "failed to download '$url'"
 
-if type sha256sum > /dev/null 2>&1; then
+if command -v sha256sum > /dev/null 2>&1; then
     hash2="$(sha256sum -b "$tarball" | cut -c1-64)"
-elif type shasum > /dev/null 2>&1; then
+elif command -v shasum > /dev/null 2>&1; then
     hash2="$(shasum -a 256 -b "$tarball" | cut -c1-64)"
-elif type openssl > /dev/null 2>&1; then
+elif command -v openssl > /dev/null 2>&1; then
     hash2="$(openssl dgst -r -sha256 "$tarball" | cut -c1-64)"
 else
     oops "cannot verify the SHA-256 hash of '$url'; you need one of 'shasum', 'sha256sum', or 'openssl'"
@@ -57,11 +63,11 @@ fi
 
 unpack=$tmpDir/unpack
 mkdir -p "$unpack"
-< "$tarball" bzcat | tar x -C "$unpack" || oops "failed to unpack '$url'"
+tar -xJf "$tarball" -C "$unpack" || oops "failed to unpack '$url'"
 
 script=$(echo "$unpack"/*/install)
 
 [ -e "$script" ] || oops "installation script is missing from the binary tarball!"
-"$script"
+"$script" "$@"
 
 } # End of wrapping
